52c7179208b3a6cb3a3b162b2365fe51a7b2000c
Nithya873 <muthyalanithya3@gmail.com>
2025-10-12 11:27:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.62%) - LeetSync

--- 1137-height-checker/height-checker.py
+++ 1137-height-checker/height-checker.py
@@ -3,9 +3,9 @@ class Solution:
         hs=sorted(heights)
         c=0
         for i in range(len(heights)):
-            for j in range(len(hs)):
-                if i==j and heights[i]!=hs[i]:
-                    c+=1
+        
+            if heights[i]!=hs[i]:
+                c+=1
         return c            
 
        
