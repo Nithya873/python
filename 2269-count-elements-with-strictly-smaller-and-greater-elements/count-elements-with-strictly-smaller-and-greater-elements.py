3db2d6c8ea07e0ed54cdece4720ab5c74c272f1a
Nithya873 <muthyalanithya3@gmail.com>
2025-10-16 11:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (36.67%) - LeetSync

---
+++ 2269-count-elements-with-strictly-smaller-and-greater-elements/count-elements-with-strictly-smaller-and-greater-elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countElements(self, nums: List[int]) -> int:
+        nums.sort()
+        count=0
+        for i in range(len(nums)-1):
+            if nums[i]>nums[0] and nums[i]<nums[-1]:
+                count+=1
+        
+                
+        return(count)
