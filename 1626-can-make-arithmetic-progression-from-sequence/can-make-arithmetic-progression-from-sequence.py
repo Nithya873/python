faa2b7418a5586d58e50100eb9950fec86a74d6a
Nithya873 <muthyalanithya3@gmail.com>
2026-01-03 19:07:35 +0530

    Time: 4 ms (8.87%) | Memory: 17.7 MB (75.90%) - LeetSync

---
+++ 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        k=arr[1]-arr[0]  
+        for i in range(len(arr)-1):
+            if arr[i+1]-arr[i]!=k:
+                return False
+        return True                  
+       
+                
